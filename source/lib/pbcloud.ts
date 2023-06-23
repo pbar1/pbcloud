983e6243f51f4b86703a589d1489c0d216d4bee1
Pierce Bartine <piercebartine@gmail.com>
2023-06-23 10:34:00 -0700

    Kustomization per namespace

--- source/lib/pbcloud.ts
+++ source/lib/pbcloud.ts
@@ -1,22 +1,44 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as fluxcd from "./crds/fluxcd";
 
 export class RenderedKubeNamespace extends pulumi.ComponentResource {
   constructor(name: string, createNamespace = true) {
-    const type = "pulumi:component:RenderedKubeNamespace";
+    const type = "pbcloud:RenderedKubeNamespace";
 
+    const renderDir = `materialized/${name}`;
     const k8sRenderProvider = new k8s.Provider(name, {
-      renderYamlToDirectory: `materialized/${name}`,
+      renderYamlToDirectory: renderDir,
     });
-
-    const opts: pulumi.ComponentResourceOptions = {
+    const componentOpts: pulumi.ComponentResourceOptions = {
       providers: [k8sRenderProvider],
     };
 
-    super(type, name, {}, opts);
+    super(type, name, {}, componentOpts);
+
+    const resourceOpts: pulumi.CustomResourceOptions = { parent: this };
 
     if (createNamespace) {
-      new k8s.core.v1.Namespace(name, { metadata: { name } }, { parent: this });
+      new k8s.core.v1.Namespace(name, { metadata: { name } }, resourceOpts);
     }
+
+    newKustomization(name, renderDir, resourceOpts);
   }
+}
+function newKustomization(
+  name: string,
+  renderDir: string,
+  opts: pulumi.CustomResourceOptions
+): fluxcd.kustomize.v1beta2.Kustomization {
+  const args: fluxcd.kustomize.v1beta2.KustomizationArgs = {
+    metadata: { name, namespace: name },
+    spec: {
+      interval: "24h",
+      path: `./${renderDir}`,
+      prune: true,
+      sourceRef: { kind: "GitRepository", name: "flux-system" }, // FIXME: hardcode
+    },
+  };
+
+  return new fluxcd.kustomize.v1beta2.Kustomization(name, args, opts);
 }
