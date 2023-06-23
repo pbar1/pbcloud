e7a83b8a4d72bd1ba21a538b4670f1d68830d60b
Pierce Bartine <piercebartine@gmail.com>
2023-06-22 19:47:58 -0700

    phrasing

--- index.ts
+++ index.ts
@@ -2,7 +2,13 @@ import * as namespaces from "./source/lib/namespaces";
 
 new namespaces.certManager.Namespace(); // verified
 new namespaces.fluxSystem.Namespace();
-new namespaces.media.Namespace();
-new namespaces.unifi.Namespace(); // verified
-new namespaces.monitoring.Namespace(); // verified
 new namespaces.gitea.Namespace(); // verified
+new namespaces.media.Namespace();
+new namespaces.monitoring.Namespace(); // verified
+new namespaces.unifi.Namespace(); // verified
+
+// still need:
+// - kube-system
+// - networking
+// - external-dns
+// - rest of flux-system
--- source/lib/namespaces/index.ts
+++ source/lib/namespaces/index.ts
@@ -1,6 +1,6 @@
 export * as certManager from "./cert_manager";
 export * as fluxSystem from "./flux_system";
+export * as gitea from "./gitea";
 export * as media from "./media";
 export * as monitoring from "./monitoring";
 export * as unifi from "./unifi";
-export * as gitea from "./gitea";
