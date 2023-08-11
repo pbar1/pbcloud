44f21b0507ae5e64cb2957df959cc8a2f26091f4
Pierce Bartine <piercebartine@gmail.com>
2023-08-11 16:35:30 -0700

    [scripts] Add kick-iptables script to...kick iptables

---
+++ scripts/kick-iptables.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# K8s services became unresponsive due to CoreDNS in the cluster not working at
+# all. The links below seem to have more detail, and the script here pretty
+# much fixed it
+#
+# https://github.com/kubernetes-sigs/kubespray/issues/4674#issuecomment-507344468
+# https://www.weave.works/blog/racy-conntrack-and-dns-lookup-timeouts
+
+set -euo pipefail
+
+# On the K8s host
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+
+# From anywhere with K8s API access
+kubectl get pod -n kube-system -l k8s-app=kube-dns -o name | xargs kubectl delete
