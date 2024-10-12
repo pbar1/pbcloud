5fc3cd3c8620eca568f5760f84880a3e7e4eaa87
Pierce Bartine <piercebartine@gmail.com>
2024-10-12 11:58:01 -0700

    onepassword

--- src/k8s/namespace/onepassword.ts
+++ src/k8s/namespace/onepassword.ts
@@ -1,6 +1,16 @@
-import * as kplus from "cdk8s-plus-30";
 import { NamespaceChart } from "../util.ts";
+import { Connect } from "@pbcloud/helm/connect.ts";
 
 export function create(ns: NamespaceChart) {
-  new kplus.Service(ns, "dummy");
+  new Connect(ns, "connect", {
+    values: {
+      connect: {
+        serviceType: "ClusterIP",
+      },
+      operator: {
+        create: true,
+        autoRestart: true,
+      },
+    },
+  });
 }
