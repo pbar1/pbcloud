8f28df31e5868c6a85a2d16f6a691377d2464e16
Pierce Bartine <piercebartine@gmail.com>
2024-10-19 11:04:50 -0700

    [k8s][monitoring] proper name

--- src/k8s/namespace/monitoring.ts
+++ src/k8s/namespace/monitoring.ts
@@ -4,8 +4,10 @@ import { Kubeprometheusstack } from "@pbcloud/helm/kube-prometheus-stack.ts";
 export function create(ns: NamespaceChart) {
   // FIXME: Deploy CRDs manually first:
   // https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack#from-64-x-to-65-x
-  new Kubeprometheusstack(ns, "kube-prometheus-stack", {
+  const kpsName = "kube-prometheus-stack";
+  new Kubeprometheusstack(ns, kpsName, {
     namespace: ns.name,
+    releaseName: kpsName,
     values: {
       alertmanager: {
         enabled: false,
@@ -24,7 +26,7 @@ export function create(ns: NamespaceChart) {
               spec: {
                 storageClassName: "local-path",
                 accessModes: ["ReadWriteOnce"],
-                resources: { requests: { storage: "0Gi" /* no-op */ } },
+                resources: { requests: { storage: "1Gi" /* no-op */ } },
               },
             },
           },
