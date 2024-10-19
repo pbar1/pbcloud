62d0d396354ff5d409c40b59b642f5f191145177
Pierce Bartine <piercebartine@gmail.com>
2024-10-19 11:27:47 -0700

    [k8s][monitoring] phrasing

--- src/k8s/namespace/monitoring.ts
+++ src/k8s/namespace/monitoring.ts
@@ -4,6 +4,7 @@ import { Kubeprometheusstack } from "@pbcloud/helm/kube-prometheus-stack.ts";
 export function create(ns: NamespaceChart) {
   // FIXME: Deploy CRDs manually first:
   // https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack#from-64-x-to-65-x
+  // See `scripts/kps-deletion-order.txt` for what order to safely to delete K8s resources
   const kpsName = "kube-prometheus-stack";
   new Kubeprometheusstack(ns, kpsName, {
     namespace: ns.name,
