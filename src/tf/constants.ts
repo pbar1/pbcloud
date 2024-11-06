a28862adaff568dd91ce277a58f93d2db9da36f8
Pierce Bartine <piercebartine@gmail.com>
2024-11-05 23:09:29 -0800

    [tf] Iterate more on Cloudflare example

---
+++ src/tf/constants.ts
@@ -0,0 +1,4 @@
+export const DOMAIN = {
+  name: "xnauts.net",
+  zoneId: "0ba0d05c6ffbfa2e8b254e07f1a29982",
+};
--- src/tf/workspace/cloudflare.ts
+++ src/tf/workspace/cloudflare.ts
@@ -1,17 +1,21 @@
 import * as cf from "@cdktf/provider-cloudflare";
+import { DOMAIN } from "../constants.ts";
 import { WorkspaceStack } from "../util.ts";
 
 const CloudflareProvider = cf.provider.CloudflareProvider;
-const AccessApplication = cf.accessApplication.AccessApplication;
+const ZeroTrustAccessApplication =
+  cf.zeroTrustAccessApplication.ZeroTrustAccessApplication;
 
 export function create(ws: WorkspaceStack) {
   // Must set CLOUDFLARE_API_TOKEN
   new CloudflareProvider(ws, "cloudflare", {});
 
-  new AccessApplication(ws, "example", {
-    zoneId: "0ba0d05c6ffbfa2e8b254e07f1a29982",
-    name: "My Example App",
-    domain: "goober.xnauts.net",
-    sessionDuration: "1m",
+  new ZeroTrustAccessApplication(ws, "example", {
+    zoneId: DOMAIN.zoneId,
+    name: "ExampleApp",
+    domain: `example.${DOMAIN.name}`,
+    sessionDuration: "730h",
+    logoUrl:
+      "https://raw.githubusercontent.com/Sonarr/Sonarr/refs/heads/develop/Logo/Sonarr.svg",
   });
 }
