83b0d1371ebcb9bbbccdfb3090f6617244d4685a
Pierce Bartine <piercebartine@gmail.com>
2024-11-05 22:37:48 -0800

    [tf] Add example Cloudflare config

--- src/tf/workspace/cloudflare.ts
+++ src/tf/workspace/cloudflare.ts
@@ -1,3 +1,17 @@
+import * as cf from "@cdktf/provider-cloudflare";
 import { WorkspaceStack } from "../util.ts";
 
-export function create(ws: WorkspaceStack) {}
+const CloudflareProvider = cf.provider.CloudflareProvider;
+const AccessApplication = cf.accessApplication.AccessApplication;
+
+export function create(ws: WorkspaceStack) {
+  // Must set CLOUDFLARE_API_TOKEN
+  new CloudflareProvider(ws, "cloudflare", {});
+
+  new AccessApplication(ws, "example", {
+    zoneId: "0ba0d05c6ffbfa2e8b254e07f1a29982",
+    name: "My Example App",
+    domain: "goober.xnauts.net",
+    sessionDuration: "1m",
+  });
+}
