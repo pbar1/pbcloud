77f032ec48d82dd9dffb3a6315c5eb303169e32e
Pierce Bartine <piercebartine@gmail.com>
2024-11-06 00:06:35 -0800

    [tf] Add CF Access tag

--- src/tf/workspace/cloudflare.ts
+++ src/tf/workspace/cloudflare.ts
@@ -3,12 +3,18 @@ import { DOMAIN } from "../constants.ts";
 import { WorkspaceStack } from "../util.ts";
 
 const CloudflareProvider = cf.provider.CloudflareProvider;
+const ZeroTrustAccessTag = cf.zeroTrustAccessTag.ZeroTrustAccessTag;
 const ZeroTrustAccessApplication =
   cf.zeroTrustAccessApplication.ZeroTrustAccessApplication;
 
 export function create(ws: WorkspaceStack) {
-  // Must set CLOUDFLARE_API_TOKEN
+  // Must set CLOUDFLARE_API_TOKEN, or CLOUDFLARE_EMAIL+CLOUDFLARE_API_KEY
   new CloudflareProvider(ws, "cloudflare", {});
+
+  new ZeroTrustAccessTag(ws, "media", {
+    zoneId: DOMAIN.zoneId,
+    name: "media",
+  });
 
   new ZeroTrustAccessApplication(ws, "example", {
     zoneId: DOMAIN.zoneId,
