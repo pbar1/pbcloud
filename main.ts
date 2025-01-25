1cccbf7b529b2332948e5ca24030595eca5c9343
Pierce Bartine <piercebartine@gmail.com>
2025-01-25 12:05:42 -0800

    Demo K8s resources

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { namespace, service } from "@cdktf/provider-kubernetes";
 import { App, TerraformStack } from "cdktf";
 import { Construct } from "constructs";
 
@@ -5,7 +6,16 @@ class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new namespace.Namespace(scope, "test-ns", {
+      metadata: { name: "test" },
+    });
+
+    new service.Service(scope, "test-svc", {
+      metadata: { name: "sonarr", namespace: "test" },
+      spec: {
+        port: [{ port: 8989 }],
+      },
+    });
   }
 }
 
