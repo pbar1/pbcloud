cce86dafd94f53b384d22f3ded0961f64c823e22
Pierce Bartine <piercebartine@gmail.com>
2025-01-25 13:42:56 -0800

    Got Sonarr/Radarr working

--- clusters/tec/media.tf
+++ clusters/tec/media.tf
@@ -1,12 +1,32 @@
-resource "kubernetes_namespace" "media" {
-  metadata {
-    name = "test"
-  }
-}
+# TODO: Import state
+# resource "kubernetes_namespace" "media" {
+#   metadata {
+#     name = "media"
+#   }
+# }
 
 module "sonarr" {
   source    = "../../modules/arr-app"
-  namespace = kubernetes_namespace.media.metadata[0].name
+  namespace = "media"
   image     = "ghcr.io/hotio/sonarr:latest"
   port      = 8989
+  vol = {
+    "/zssd/general/config/sonarr" = "/config"
+    "/data/torrents"              = "/downloads"
+    "/data/media/tv"              = "/tv"
+    "/data/media/recycle-bin"     = "/recycle-bin"
+  }
+}
+
+module "radarr" {
+  source    = "../../modules/arr-app"
+  namespace = "media"
+  image     = "ghcr.io/hotio/radarr:latest"
+  port      = 7878
+  vol = {
+    "/zssd/general/config/radarr" = "/config"
+    "/data/torrents"              = "/downloads"
+    "/data/media/movies"          = "/movies"
+    "/data/media/recycle-bin"     = "/recycle-bin"
+  }
 }
--- modules/arr-app/mod.tf
+++ modules/arr-app/mod.tf
@@ -1,13 +1,28 @@
 variable "namespace" {
-  type = string
+  type        = string
+  description = "Kubernetes namespace"
 }
 
 variable "image" {
-  type = string
+  type        = string
+  description = "Container image to run"
 }
 
 variable "port" {
-  type = number
+  type        = number
+  description = "Port to be exposed"
+}
+
+variable "env" {
+  type        = map(string)
+  description = "Container environment variables"
+  default     = {}
+}
+
+variable "vol" {
+  type        = map(string)
+  description = "Host path to container path mappings"
+  default     = {}
 }
 
 locals {
@@ -23,6 +38,22 @@ locals {
 
   labels = {
     "app" : local.name
+  }
+
+  env_default = {
+    PUID = "1000"
+    PGID = "100"
+    TZ   = "America/Los_Angeles"
+  }
+
+  env = merge(local.env_default, var.env)
+
+  vol = {
+    for host_path, container_path in var.vol :
+    element(compact(split("/", container_path)), -1) => {
+      host_path      = host_path
+      container_path = container_path
+    }
   }
 }
 
@@ -60,6 +91,29 @@ resource "kubernetes_stateful_set" "app" {
           image = var.image
           port {
             container_port = var.port
+          }
+          dynamic "env" {
+            for_each = local.env
+            content {
+              name  = env.key
+              value = env.value
+            }
+          }
+          dynamic "volume_mount" {
+            for_each = local.vol
+            content {
+              name       = volume_mount.key
+              mount_path = volume_mount.value.container_path
+            }
+          }
+        }
+        dynamic "volume" {
+          for_each = local.vol
+          content {
+            name = volume.key
+            host_path {
+              path = volume.value.host_path
+            }
           }
         }
       }
